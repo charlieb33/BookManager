df6fffabe319a12976385b716cf1cb8a1a2cc9cf
Charles Beach <charlesbeach96@yahoo.com>
2024-05-19 15:31:57 -0400

    Delete BookCoverApi and modify BookDetailsApi

---
+++ book-manager/src/utils/BookDetailsApi.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export const fetchBookDetails = async (isbn) => {
+    try {
+        const response = await axios.get(`https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&jscmd=data&format=json`);
+        return response.data[`ISBN:${isbn}`];
+    } catch (error) {
+        throw new Error("Failed to retrieve book data");
+    }
+};
